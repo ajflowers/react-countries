be337f4f0a86c1a1dcd6194a0c91c356e829db9d
Angela Flowers <me@angela.flowers>
2021-05-04 16:55:22 -0400

    react router install

--- countries/src/App.js
+++ countries/src/App.js
@@ -4,21 +4,9 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mx-auto w-2/3 bg-gray-400">
+      <p>test</p>
+      
     </div>
   );
 }
